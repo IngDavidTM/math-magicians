4ca3e42817eccfa8d3320e8a2219dde66df8a6c4
IngDavidTM <david-1011@hotmail.com>
2022-10-24 21:35:56 -0500

    add calculator.js file

---
+++ src/components/Calculator.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/prefer-stateless-function */
+import './Calculator.css';
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+        <p className="screen">0</p>
+        <div className="keys">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="orange">÷</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="orange">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="orange">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="orange">+</button>
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button" className="orange">=</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
