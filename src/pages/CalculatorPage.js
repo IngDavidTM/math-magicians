545c9cf3a45c5527108352d1b1b2ba85b37f6089
IngDavidTM <david-1011@hotmail.com>
2022-11-01 11:23:12 -0500

    add style to calculatorPage

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../stylesheet/CalculatorPage.css';
 import Calculator from '../components/Calculator';
 
 const CalculatorPage = () => (
-  <div>
+  <div className="calculatorPage">
     <h2>Lets do some math!</h2>
     <Calculator />
   </div>
