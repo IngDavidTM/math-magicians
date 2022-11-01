93a47cb0101e4e1dfecb9da63093dd707f3cf214
IngDavidTM <david-1011@hotmail.com>
2022-11-01 11:29:46 -0500

    add style to quote

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../stylesheet/Quote.css';
 
 const Quote = () => (
-  <div>
+  <div className="quote">
     <h2>
       Mathemathics is not about numbers, equations, computations, or algorithms;
       it is about undertanding. -William Paul Thurston
