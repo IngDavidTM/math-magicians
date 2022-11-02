37282ac8392054a92a05e8fdc2c7a7415e3de952
StarMindz <64619873+StarMindz@users.noreply.github.com>
2022-11-02 20:43:27 +0100

    Add test

--- src/__tests__/CalculatorPage.test.js
+++ src/__tests__/CalculatorPage.test.js
@@ -1,7 +1,7 @@
-import renderer from "react-test-renderer";
-import CalculatorPage from "./pages/CalculatorPage";
+import renderer from 'react-test-renderer';
+import CalculatorPage from './pages/CalculatorPage';
 
-it("should render CalculatorPage", () => {
+it('should render CalculatorPage', () => {
   const renderCalculatorPage = renderer.create(<CalculatorPage />).toJSON();
   expect(renderCalculatorPage).toMatchSnapshot();
 });
