a406b5fa804c13a7365320dd33df5361adbb0940
IngDavidTM <david-1011@hotmail.com>
2022-11-02 21:32:30 -0500

    add test library to Calculator

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,7 +1,51 @@
 import renderer from 'react-test-renderer';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Calculator from '../components/Calculator';
 
-it('should render Calculator', () => {
-  const renderCalculator = renderer.create(<Calculator />).toJSON();
-  expect(renderCalculator).toMatchSnapshot();
+describe('test snapshop', () => {
+  it('should render Calculator', () => {
+    const renderCalculator = renderer.create(<Calculator />).toJSON();
+    expect(renderCalculator).toMatchSnapshot();
+  });
+});
+
+describe('test with testing library', () => {
+  test('should work for addition', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByText('16')).toBeInTheDocument();
+  });
+
+  test('should work for subtraction', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('9'));
+    fireEvent.click(screen.getByText('8'));
+    fireEvent.click(screen.getByText('-'));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByText('96')).toBeInTheDocument();
+  });
+
+  test('should work for multiplication', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('x'));
+    fireEvent.click(screen.getByText('3'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByText('15')).toBeInTheDocument();
+  });
+
+  test('should work for division', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByText('3'));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('÷'));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByText('16')).toBeInTheDocument();
+  });
 });
--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -2,9 +2,11 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
-it('should render NavBar', () => {
-  const renderNavBar = renderer
-    .create(<BrowserRouter><NavBar /></BrowserRouter>)
-    .toJSON();
-  expect(renderNavBar).toMatchSnapshot();
+describe('test snapshop', () => {
+  it('should render NavBar', () => {
+    const renderNavBar = renderer
+      .create(<BrowserRouter><NavBar /></BrowserRouter>)
+      .toJSON();
+    expect(renderNavBar).toMatchSnapshot();
+  });
 });
